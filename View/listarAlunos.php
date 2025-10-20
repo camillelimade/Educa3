f36d3908d15e0c13aaba49a692fe95ee6dcc8181
camillelimade <138686617+camillelimade@users.noreply.github.com>
2025-10-20 09:03:50 -0300

    filtro de alunos por serie funcional

--- View/listarAlunos.php
+++ View/listarAlunos.php
@@ -1,77 +1,87 @@
 <?php
-    //Incluindo conexao com banco de dados
-    include_once '../Controller/CConexao.php';
-    $conexao = new CConexao();
-    $conn = $conexao->getConnection();
+include_once '../Controller/CConexao.php';
+$conexao = new CConexao();
+$conn = $conexao->getConnection();
 
-    //Recebendo os dados da requisição
-    $dados_requisicao = $_REQUEST;
+$dados_requisicao = $_REQUEST;
 
-    //Obtendo registros
-    $query_Quantidade_Alunos = "SELECT COUNT(idAluno) AS Quantidade_al FROM aluno";
-    
-    $result_Quantidade_Alunos = $conn->prepare($query_Quantidade_Alunos);
+// Filtro por turma (recebido via GET do DataTables)
+$where = "";
+if (!empty($_GET['turma']) && $_GET['turma'] != "   ") {
+    $turma = intval($_GET['turma']);
+    $where = "WHERE aluno.Turma_idTurma = :turma";
+}
 
-    $result_Quantidade_Alunos->execute();
-    $row_Quantidade_Alunos = $result_Quantidade_Alunos->fetch(PDO::FETCH_ASSOC);
+// Contar total de alunos
+$query_Quantidade_Alunos = "SELECT COUNT(idAluno) AS Quantidade_al FROM aluno";
+$result_Quantidade_Alunos = $conn->prepare($query_Quantidade_Alunos);
+$result_Quantidade_Alunos->execute();
+$row_Quantidade_Alunos = $result_Quantidade_Alunos->fetch(PDO::FETCH_ASSOC);
 
-        $sql = "SELECT 
-        aluno.NomeAluno,
-        aluno.idAluno,
-        aluno.Turma_idTurma,
-        aluno.EmailAluno,
-        'aluno' AS tipo,
-        turma.nomeTurma,
-        turma.AnoTurma
-    FROM aluno
-    LEFT JOIN turma ON aluno.Turma_idTurma = turma.idTurma";
+// Consulta principal
+$sql = "SELECT 
+            aluno.NomeAluno,
+            aluno.idAluno,
+            aluno.Turma_idTurma,
+            aluno.EmailAluno,
+            turma.nomeTurma,
+            turma.AnoTurma
+        FROM aluno
+        LEFT JOIN turma ON aluno.Turma_idTurma = turma.idTurma
+        $where";
 
-    if(!empty($dados_requisicao['search']['value'])){
-        $sql .= " WHERE aluno.NomeAluno LIKE :NomeAluno ";
-        $sql .= " OR aluno.idAluno LIKE :idAluno ";
+// Se houver pesquisa, adiciona filtro
+if (!empty($dados_requisicao['search']['value'])) {
+    $valor_pesq = "%" . $dados_requisicao['search']['value'] . "%";
+    if ($where == "") {
+        $sql .= " WHERE aluno.NomeAluno LIKE :NomeAluno OR aluno.idAluno LIKE :idAluno";
+    } else {
+        $sql .= " AND (aluno.NomeAluno LIKE :NomeAluno OR aluno.idAluno LIKE :idAluno)";
     }
-    
-    $sql .= " ORDER BY aluno.NomeAluno LIMIT :inicio , :quantidade";
+}
 
-    $result_Alunos = $conn->prepare($sql); 
+// Ordenação e paginação
+$sql .= " ORDER BY aluno.NomeAluno LIMIT :inicio , :quantidade";
+$result_Alunos = $conn->prepare($sql);
 
-    $result_Alunos->bindParam(':inicio',$dados_requisicao['start'],PDO::PARAM_INT);
-    $result_Alunos->bindParam(':quantidade',$dados_requisicao['length'],PDO::PARAM_INT);
+$result_Alunos->bindParam(':inicio', $dados_requisicao['start'], PDO::PARAM_INT);
+$result_Alunos->bindParam(':quantidade', $dados_requisicao['length'], PDO::PARAM_INT);
 
-    if(!empty($dados_requisicao['search']['value'])){
-        $valor_pesq = "%" . $dados_requisicao['search']['value'] . "%";
-        $result_Alunos->bindParam(':NomeAluno', $valor_pesq, PDO::PARAM_STR);
-        $result_Alunos->bindParam(':idAluno', $valor_pesq, PDO::PARAM_STR);
-    }
+// Liga o parâmetro da turma, se existir
+if (!empty($_GET['turma']) && $_GET['turma'] != "0") {
+    $result_Alunos->bindParam(':turma', $turma, PDO::PARAM_INT);
+}
 
-    //Executar a Query
-    $result_Alunos->execute();
+// Liga os parâmetros da pesquisa, se existirem
+if (!empty($dados_requisicao['search']['value'])) {
+    $result_Alunos->bindParam(':NomeAluno', $valor_pesq, PDO::PARAM_STR);
+    $result_Alunos->bindParam(':idAluno', $valor_pesq, PDO::PARAM_STR);
+}
 
-    while($row_al = $result_Alunos->fetch(PDO::FETCH_ASSOC)) {
-        //var_dump($row_Emp);
-        extract($row_al);
-        $registro = [];
-        $registro[] = $idAluno;
-        $registro[] = $NomeAluno;
-        $registro[] = $AnoTurma;
-        $registro[] = $nomeTurma;
-        $registro[] = $EmailAluno;
-        $registro[] = "<button type='button' class='btn btn-outline-warning' id='$idAluno' onclick='editAluno($idAluno)'><i class='fas fa-pen'></i></button>";
-        $registro[] = " <button type='button' class='btn btn-outline-danger' id='$idAluno' onclick='excluirAluno($idAluno)'><i class='fas fa-trash-alt'></i></button>";
+// Executar consulta
+$result_Alunos->execute();
 
-        $dados[] = $registro;
-    }
-    //var_dump($dados);
+$dados = [];
+while ($row_al = $result_Alunos->fetch(PDO::FETCH_ASSOC)) {
+    extract($row_al);
+    $registro = [];
+    $registro[] = $idAluno;
+    $registro[] = $NomeAluno;
+    $registro[] = $AnoTurma;
+    $registro[] = $nomeTurma;
+    $registro[] = $EmailAluno;
+    $registro[] = "<button type='button' class='btn btn-outline-warning' id='$idAluno' onclick='editAluno($idAluno)'><i class='fas fa-pen'></i></button>";
+    $registro[] = "<button type='button' class='btn btn-outline-danger' id='$idAluno' onclick='excluirAluno($idAluno)'><i class='fas fa-trash-alt'></i></button>";
 
-    //Criando array de informações a serem retornadas para o JavaScript
-    $resultado =[
-        "draw" => intval($dados_requisicao['draw']),
-        "recordsTotal" => intval($row_Quantidade_Alunos['Quantidade_al']), //Quantidade de registros
-        "recordsFiltered" => intval($row_Quantidade_Alunos['Quantidade_al']), //Total de registros quando tiver pesquisa
-        "data" => $dados //Array de dados com o registro
-    ];
-    //var_dump($resultado);
+    $dados[] = $registro;
+}
 
-    $var = json_encode($resultado);
-    echo $var;
+$resultado = [
+    "draw" => intval($dados_requisicao['draw']),
+    "recordsTotal" => intval($row_Quantidade_Alunos['Quantidade_al']),
+    "recordsFiltered" => intval($row_Quantidade_Alunos['Quantidade_al']),
+    "data" => $dados
+];
+
+echo json_encode($resultado);
 ?>
